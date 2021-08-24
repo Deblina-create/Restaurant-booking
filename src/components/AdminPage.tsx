5f75b78a8c895d34370e4aaaf2fc38dcfffea0fa
Sumi <ksm2920@gmail.com>
2021-08-24 15:00:55 +0200

    display booking list with mock data and some styling

---
+++ src/components/AdminPage.tsx
@@ -0,0 +1,38 @@
+import { getMaxListeners } from "process";
+import { useState } from "react";
+import { getCombinedModifierFlags } from "typescript";
+import "../Admin.css";
+
+// import { Link } from "react-router-dom";
+export const AdmingPage = () => {
+  let defaultValue = [
+    { id: 1, BookingTime: "18:00", NoOfPeople: 2, Name: "Stina" },
+    { id: 2, BookingTime: "18:00", NoOfPeople: 4, Name: "Per" },
+    { id: 3, BookingTime: "18:00", NoOfPeople: 3, Name: "Sara" },
+    { id: 4, BookingTime: "18:00", NoOfPeople: 6, Name: "Nils" },
+  ];
+  const [bookings, setBookings] = useState(defaultValue);
+  let liTag = bookings.map((booking) => {
+    return (
+      <li className="booking-list">
+        <span>{booking.BookingTime}</span>
+        <span>{booking.NoOfPeople}</span>
+        <span>{booking.Name}</span>
+      </li>
+    );
+  });
+  return (
+    <div className="admin-page">
+      <div className="back">
+        {/* <Link to={"/"}><i></i> Admin</Link> */}
+        <a href="/">
+          <span>Admin</span>
+        </a>
+      </div>
+      <div>
+        <p className="add">add</p>
+        <ul>{liTag}</ul>
+      </div>
+    </div>
+  );
+};
