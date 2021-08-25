7281d42ce4b23501af980eb3fd7d74d79a9a991b
Sumi <ksm2920@gmail.com>
2021-08-25 20:46:37 +0200

    add increment buttons to change date

---
+++ src/components/Admin.test.ts
@@ -0,0 +1,7 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { AdmingPage } from "./AdminPage";
+
+// it("renders correctly", () => {
+//     const {queryByTestId, queryByPlaceholderName} = render(<>)
+// })
--- src/components/AdminPage.tsx
+++ src/components/AdminPage.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "../Admin.css";
 import { DeleteModal } from "../modals/DeleteModal";
-import { EditModal} from "../modals/EditModal";
+import { EditModal } from "../modals/EditModal";
 
 // import { Link } from "react-router-dom";
 
@@ -14,23 +14,34 @@ export const AdmingPage = () => {
   ];
 
   let dateNow = new Date().toDateString();
+  let currentDate = new Date();
+  let numberOfMlSeconds = currentDate.getTime();
+  let dayToMlSeconds = 24 * 60 * 60 * 1000;
+  let previousDate = new Date(numberOfMlSeconds - dayToMlSeconds).toDateString();
+  let nextDate = new Date(numberOfMlSeconds + dayToMlSeconds).toDateString();
 
   const [bookings, setBookings] = useState(defaultValue);
   const [showEdit, setShowEdit] = useState(false);
   const [showDelete, setShowDelete] = useState(false);
   const [date, setDate] = useState(dateNow);
 
-  let totalNoOfPeople = bookings.reduce((acc, curr) => acc + curr.NoOfPeople, 0);
+  let totalNoOfPeople = bookings.reduce(
+    (acc, curr) => acc + curr.NoOfPeople,
+    0
+  );
 
   let liTag = bookings.map((booking) => {
     return (
-      <li className="booking-list">
+      <li key={booking.id} className="booking-list">
         <span>{booking.BookingTime}</span>
         <span>{booking.NoOfPeople}</span>
         <span>{booking.Name}</span>
-
-        <button onClick={() => setShowEdit(true)}>edit</button>
-        <button onClick={() => setShowDelete(true)}>delete</button>
+        <button onClick={() => setShowEdit(true)} className="edit-icon">
+          <i className="fas fa-pen"></i>
+        </button>
+        <button onClick={() => setShowDelete(true)} className="remove-icon">
+          <i className="fas fa-trash-alt"></i>
+        </button>
       </li>
     );
   });
@@ -40,22 +51,37 @@ export const AdmingPage = () => {
       <div className="back">
         {/* <Link to={"/"}><i></i> Admin</Link> */}
         <a href="/">
-        <span><i className="fas fa-chevron-left"></i></span>
-        <span> Admin</span>
+          <span>
+            <i className="fas fa-chevron-left"></i>
+          </span>
+          <span> Admin</span>
         </a>
       </div>
       <div>
-        <h2>{date}</h2>
+        <h2>
+          {date} (Today)
+          <button onClick={() => setDate(previousDate)}>
+            <i className="fas fa-chevron-left"></i>
+          </button>
+          <button onClick={() => setDate(nextDate)}>
+            <i className="fas fa-chevron-right"></i>
+          </button>
+        </h2>
       </div>
-      <p className="total">Total:  {bookings.length} bookings and {totalNoOfPeople} people</p>
+      <p className="total">
+        Total: {bookings.length} bookings and {totalNoOfPeople} people
+      </p>
       <div className="add">
-        <button className="add-button"><i className="fas fa-plus"></i><span> add</span></button>
+        <button className="add-icon">
+          <i className="fas fa-plus"></i>
+          <span> add</span>
+        </button>
       </div>
       <div>
-      <ul>{liTag}</ul>
+        <ul>{liTag}</ul>
       </div>
       <EditModal onClose={() => setShowEdit(false)} show={showEdit} />
-      <DeleteModal onClose={() => setShowDelete(false)} show={showDelete}/>
+      <DeleteModal onClose={() => setShowDelete(false)} show={showDelete} />
     </div>
   );
 };
