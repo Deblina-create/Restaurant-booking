3479821d6477b6302222021891ee13103846600e
emresorkun <emresorkun@gmail.com>
2021-08-27 10:33:49 +0200

    First test completed

--- src/components/__test__/P.test.js
+++ src/components/__test__/P.test.js
@@ -3,8 +3,15 @@ import { render, screen } from '@testing-library/react';
 import ContactForm from '../ContactForm';
 
 
-test('should render same name entered by the contact form', async () => {
-  render(<ContactForm title="tatat"/>);
-  const customerNameElement = screen.getByText(/tatat/i);
-  expect(customerNameElement).toBeInTheDocument();
+test('should check if the page rendered as it should be-contact form', async () => {
+  render(<ContactForm />);
+  const contactFormSentence = screen.getByText(/Please Contact Us/i);
+  expect(contactFormSentence).toBeInTheDocument();
 });
+
+
+test('should check if the page rendered as it should be-message recieved', async () => {
+    render(<ContactForm />);
+    const messageRecieved = screen.getByText(/Your Message was Recieved!/i);
+    expect(messageRecieved).toBeInTheDocument();
+  });
