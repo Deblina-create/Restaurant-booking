a6d2ad4cbcb0a95f35411fcb52e7f306d9e07990
Sumi <ksm2920@gmail.com>
2021-08-30 20:20:28 +0200

    add some styling to link to booking-page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import "./Home.css";
-import Table_logo from "./The_Table_logo.png"
+import Table_logo from "./The_Table_logo.png";
 
 export const Home = () => {
   return (
@@ -9,15 +9,24 @@ export const Home = () => {
         <img src={Table_logo} alt="restaurant-logo" className="logo-img"></img>
       </div>
       <div className="bookingLink">
-        <Link to="/booking"><h1 id="book">BOOK <span id="table">YOUR TABLE</span></h1>
-        </Link>        
+        <Link to="/booking">
+          <h1 id="book">
+            <span>BOOK </span>
+            <span id="table"> YOUR TABLE </span>
+            <span id="here"> HERE </span>
+          </h1>
+        </Link>
       </div>
       <div className="links">
         <div>
-          <Link to="/contact">Contact us <i className="fas fa-chevron-down"></i></Link>
+          <Link to="/contact">
+            Contact us <i className="fas fa-chevron-down"></i>
+          </Link>
         </div>
         <div>
-          <Link to="/admin">Admin <i className="fas fa-chevron-down"></i></Link>
+          <Link to="/admin">
+            Admin <i className="fas fa-chevron-down"></i>
+          </Link>
         </div>
       </div>
     </div>
