2edc7451798e9997192e5860b583ed42af9027df
Sumi <ksm2920@gmail.com>
2021-08-31 18:31:46 +0200

    can get data by id

--- src/components/EditForm.tsx
+++ src/components/EditForm.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useHistory, useParams } from "react-router-dom";
 import restaurantApi from "../api/restaurantApi";
 import Booking from "../models/Booking";
 import ErrorResponse from "../models/ErrorResponse";
@@ -20,128 +20,137 @@ const initialBookingInfo: Booking = {
 
 export const EditForm = () => {
   const { id } = useParams<editParams>();
- 
-  const [bookingInfo, setBookingInfo] = useState(initialBookingInfo);
-  const [changeNoOfPeople, setChangeNoOfPeople] = useState(bookingInfo.NoOfPeople);
-//   let date = new Date(bookingInfo.BookingTime);
-//   let bookedDate = date.toISOString().split("T")[0];
-//   const [changeDate, setChangeDate] = useState(bookedDate);
-  const [changeName, setChangeName] = useState(bookingInfo.Name);
-  const [changePhone, setChangePhone] = useState(bookingInfo.Phone);
-  const [changeEmail, setChangeEmail] = useState(bookingInfo.Email);
-//   let bookedTime = date.toLocaleTimeString("sv-SE", { timeStyle: "short" });
-//   const [changeTime, setChangeTime] = useState(bookedTime);
 
-   
+  const [bookingInfo, setBookingInfo] = useState(initialBookingInfo);
+  const [bookedDate, setBookedDate] = useState("");
+  const [bookedTime, setBookedTime] = useState("");
+
+  const history = useHistory();
 
   const fetchData = async () => {
     const res = await restaurantApi.get<Booking | null>(`/booking/${id}`);
-    setBookingInfo(res.data as Booking);
+    let booking = res.data as Booking;
+    setBookingInfo(booking);
     console.log(res.data);
+    let date = new Date(booking.BookingTime);
+    setBookedDate(date.toISOString().split("T")[0]);
+    setBookedTime(date.toLocaleTimeString("sv-SE", { timeStyle: "short" }));
   };
 
   const saveData = async () => {
-    // bookingTime();
+    bookingTime();
     console.log(bookingInfo);
     await restaurantApi.put<Booking | ErrorResponse>("/booking", {
       data: bookingInfo,
     });
+    history.push('/admin');
   };
 
   useEffect(() => {
     fetchData();
-    console.log(bookingInfo);
   }, []);
 
+  const onNumberOfPeopleChange = (e: any) => {
+    setBookingInfo({
+      ...bookingInfo,
+      NoOfPeople: Number.parseInt(e.target.value.toString()),
+    });
+  };
+
+  const nameChanged = (e: any) => {
+    const name = e.target.value.toString();
+    setBookingInfo({ ...bookingInfo, Name: name });
+  };
+
+  const phoneChanged = (e: any) => {
+    setBookingInfo({ ...bookingInfo, Phone: e.target.value.toString() });
+  };
+
+  const emailChanged = (e: any) => {
+    const email = e.target.value.toString();
+    setBookingInfo({ ...bookingInfo, Email: email });
+  };
+
   return (
-    
-      <div className="admin-page">
-        <div>
-          <h3>Booking detail</h3>
+    <div className="admin-page">
+      <div>
+        <h3>Edit form</h3>
+      </div>
+      <div>
+        <input 
+        type="date" 
+        defaultValue={bookedDate}
+        onChange={(e) => setBookedDate(e.target.value)}
+         />
+        <div className="radio">
+          <div className="radio-btn">
+            <input
+              id="time18"
+              type="radio"
+              value={"18:00"}
+              name="time"
+              defaultChecked={bookedTime === "18:00"}
+              onChange={(e) => setBookedTime(e.target.value)}
+            />
+            <label htmlFor="time18">18:00</label>
+          </div>
+          <div className="radio-btn">
+            <input
+              id="time21"
+              type="radio"
+              value={"21:00"}
+              name="time"
+              defaultChecked={bookedTime === "21:00"}
+              onChange={(e) => setBookedTime(e.target.value)}
+            />
+            <label htmlFor="time21">21:00</label>
+          </div>
         </div>
-        <div>
-          <input
-            type="text"
-            // defaultValue={changeDate}
-            // onChange={(e) => (setChangeDate(e.target.value))}
-          />
-          {/* <div className="radio">
-            <div className="radio-btn">
-              <input
-                id="time18"
-                type="radio"
-                value={"18:00"}
-                name="time"
-                onChange={(e) => {
-                  bookedTime = e.target.value;
-                }}
-                defaultChecked={bookedTime === "18:00"}
-              />
-              <label htmlFor="time18">18:00</label>
-            </div>
-            <div className="radio-btn">
-              <input
-                id="time21"
-                type="radio"
-                value={"21:00"}
-                name="time"
-                onChange={(e) => {
-                  bookedTime = e.target.value;
-                }}
-                defaultChecked={bookedTime === "21:00"}
-              />
-              <label htmlFor="time21">21:00</label>
-            </div>
-          </div> */}
-          <input
-            type="number"
-            placeholder="Number of people"
-            defaultValue={changeNoOfPeople}
-            onChange={(e) =>
-              (setChangeNoOfPeople(Number.parseInt(
-                e.target.value.toString())
-              ))
-            }
-          />
-          <input
-            type="text"
-            placeholder="Name"
-            defaultValue={changeName}
-            onChange={(e) => (setChangeName(e.target.value))}
-          />
-          <input
-            type="text"
-            placeholder="Mobile number"
-            defaultValue={changePhone}
-            onChange={(e) => (setChangePhone(e.target.value))}
-          />
-          <input
-            type="text"
-            placeholder="Email"
-            defaultValue={changeEmail}
-            onChange={(e) => (setChangeEmail(e.target.value))}
-          />
-          {/* <input
+        <input
+          type="number"
+          placeholder="Number of people"
+          defaultValue={bookingInfo.NoOfPeople}
+          onChange={onNumberOfPeopleChange}
+        />
+        <input
+          type="text"
+          placeholder="Name"
+          defaultValue={bookingInfo.Name}
+          onChange={nameChanged}
+        />
+        <input
+          type="text"
+          placeholder="Mobile number"
+          defaultValue={bookingInfo.Phone}
+          onChange={phoneChanged}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          defaultValue={bookingInfo.Email}
+          onChange={emailChanged}
+        />
+        {/* <input
             type="text"
             placeholder="Preference"
             defaultValue={bookingInfo.Preference}
           /> */}
-        </div>
-        <div className="modal-footer">
-          <button onClick={saveData} className="full-btn">
-            Save
-          </button>
-        </div>
       </div>
+      <div className="modal-footer">
+        <button onClick={saveData} className="full-btn">
+          Save
+        </button>
+      </div>
+    </div>
   );
-    // function bookingTime() {
-    //   const dt = new Date(bookedDate.toString());
-    //   let bookingTimeText = "";
-    //   if (bookedTime === "18:00") {
-    //     bookingTimeText = new Date(new Date(dt).setHours(18, 0, 0, 0)).toString();
-    //   } else if (bookedTime === "21:00") {
-    //     bookingTimeText = new Date(new Date(dt).setHours(21, 0, 0, 0)).toString();
-    //   }
-    //   bookingInfo.BookingTime = bookingTimeText;
-    // }
+  function bookingTime() {
+    const dt = new Date(bookedDate.toString());
+    let bookingTimeText = "";
+    if (bookedTime === "18:00") {
+      bookingTimeText = new Date(new Date(dt).setHours(18, 0, 0, 0)).toString();
+    } else if (bookedTime === "21:00") {
+      bookingTimeText = new Date(new Date(dt).setHours(21, 0, 0, 0)).toString();
+    }
+    setBookingInfo({ ...bookingInfo, BookingTime: bookingTimeText });
+  }
 };
