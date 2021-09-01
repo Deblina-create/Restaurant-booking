237cdb0f5891ec1636be20ac321c65c0d7a52841
Sumi <ksm2920@gmail.com>
2021-09-01 08:44:03 +0200

    change styling for disabled

--- src/components/EditForm.tsx
+++ src/components/EditForm.tsx
@@ -3,6 +3,9 @@ import { Link, useHistory, useParams } from "react-router-dom";
 import restaurantApi from "../api/restaurantApi";
 import Booking from "../models/Booking";
 import ErrorResponse from "../models/ErrorResponse";
+import SearchInfo from "../models/SearchInfo";
+import SearchRequest from "../models/SearchRequest";
+import "./Admin.css";
 
 type editParams = {
   id: string;
@@ -18,23 +21,56 @@ const initialBookingInfo: Booking = {
   BookedTableCount: 0,
 };
 
+const initialData: SearchInfo[] = [];
+const initialSelectedSlot: SearchInfo = {
+  TimeSlotIndex: -1,
+  TimeSlotText: "",
+  IsTableAvailable: false,
+};
+
 export const EditForm = () => {
   const { id } = useParams<editParams>();
 
   const [bookingInfo, setBookingInfo] = useState(initialBookingInfo);
   const [bookedDate, setBookedDate] = useState("");
   const [bookedTime, setBookedTime] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const [disabledBtn, setDisabledBtn] = useState(false);
+  const [searchData, setSearchData] = useState(initialData);
+  const [dataFetched, setDataFetched] = useState(false);
+  const [selectedSlot, setSelectedSlot] = useState(initialSelectedSlot);
+  const [type, setType] = useState("radio");
+  const [hidden, setHidden] = useState(false);
 
   const history = useHistory();
 
   const fetchData = async () => {
     const res = await restaurantApi.get<Booking | null>(`/booking/${id}`);
-    let booking = res.data as Booking;
-    setBookingInfo(booking);
+    let selectedBooking = res.data as Booking;
+    setBookingInfo(selectedBooking);
     console.log(res.data);
-    let date = new Date(booking.BookingTime);
+    let date = new Date(selectedBooking.BookingTime);
     setBookedDate(date.toISOString().split("T")[0]);
     setBookedTime(date.toLocaleTimeString("sv-SE", { timeStyle: "short" }));
+  };
+
+  const searchTable = async () => {
+    setType("hidden");
+    setHidden(true);
+    let peopleCount = bookingInfo.NoOfPeople;
+    console.log("Date value", bookedDate);
+    console.log("People value", peopleCount);
+    const payload: SearchRequest = {
+      BookingDate: new Date(bookedDate),
+      PeopleCount: peopleCount,
+    };
+    console.log("Payload", payload);
+    const x = await restaurantApi.post<SearchInfo[]>("/search", {
+      data: payload,
+    });
+    setSearchData(x.data as SearchInfo[]);
+    setDataFetched(true);
+    console.log("response data", x.data);
   };
 
   const saveData = async () => {
@@ -71,47 +107,97 @@ export const EditForm = () => {
     setBookingInfo({ ...bookingInfo, Email: email });
   };
 
+  const activate = () => {
+    setDisabled(false);
+    // setDisabledBtn(true);
+  };
+
+  const openForm = (timeSlot: SearchInfo) => {
+    setSelectedSlot(timeSlot);
+  };
+
   return (
     <div className="admin-page">
-      <div>
-        <h3>Edit form</h3>
+      <div className="back">
+        <Link to={"/admin"}>
+          <i className="fas fa-chevron-left"></i> Edit form
+        </Link>
       </div>
       <div>
+        <div>
+          <p>
+            Booking information
+            <span className="edit" onClick={activate}>
+              <i className="fas fa-pen"></i>
+            </span>
+          </p>
+        </div>
+
         <input
           type="date"
           value={bookedDate}
           onChange={(e) => setBookedDate(e.target.value)}
+          disabled={disabled}
         />
-        <div className="radio">
-          <div className="radio-btn">
-            <input
-              id="time18"
-              type="radio"
-              value={"18:00"}
-              name="time"
-              checked={bookedTime === "18:00"}
-              onChange={(e) => setBookedTime(e.target.value)}
-            />
-            <label htmlFor="time18">18:00</label>
-          </div>
-          <div className="radio-btn">
-            <input
-              id="time21"
-              type="radio"
-              value={"21:00"}
-              name="time"
-              checked={bookedTime === "21:00"}
-              onChange={(e) => setBookedTime(e.target.value)}
-            />
-            <label htmlFor="time21">21:00</label>
-          </div>
-        </div>
         <input
           type="number"
           placeholder="Number of people"
           value={bookingInfo.NoOfPeople}
           onChange={onNumberOfPeopleChange}
+          disabled={disabled}
         />
+        <div className="radio">
+          {searchData.map((data, index) => (
+            <div className="radio-btn" key={index}>
+              <input
+                type="radio"
+                value={data.TimeSlotText}
+                disabled={!data.IsTableAvailable}
+                name="slot"
+                onClick={() => openForm(data)}
+              />
+              <label>{data.TimeSlotText}</label>
+            </div>
+          ))}
+        </div>
+        <div className="radio">
+          <div className="radio-btn">
+            <input
+              id="time18"
+              type={type}
+              value={"18:00"}
+              name="time"
+              checked={bookedTime === "18:00"}
+              onChange={(e) => setBookedTime(e.target.value)}
+              disabled={disabled}
+            />
+            <label htmlFor="time18" hidden={hidden}>18:00</label>
+          </div>
+          <div className="radio-btn">
+            <input
+              id="time21"
+              type={type}
+              value={"21:00"}
+              name="time"
+              checked={bookedTime === "21:00"}
+              onChange={(e) => setBookedTime(e.target.value)}
+              disabled={disabled}
+            />
+            <label htmlFor="time21" hidden={hidden}>21:00</label>
+          </div>
+        </div>
+        <div>
+          <button
+            className="empty-btn"
+            id="activate"
+            style={{ backgroundColor: "black" }}
+            disabled={disabled}
+            onClick={searchTable}
+          >
+            Search a table
+          </button>
+        </div>
+        <p>Contact information</p>
         <input
           type="text"
           placeholder="Name"
@@ -137,7 +223,7 @@ export const EditForm = () => {
           /> */}
       </div>
       <div className="modal-footer">
-        <button onClick={saveData} className="full-btn">
+        <button onClick={saveData} className="full-btn" disabled={disabledBtn}>
           Save
         </button>
       </div>
