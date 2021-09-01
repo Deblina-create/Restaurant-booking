4b751efb2aea392008ce39e982d83b21882da7df
Sumi <ksm2920@gmail.com>
2021-09-01 21:01:21 +0200

    add some testing code

--- src/components/__test__/Admin.test.js
+++ src/components/__test__/Admin.test.js
@@ -1,9 +1,10 @@
 import React from "react";
 import {render, fireEvent} from "@testing-library/react";
 import { AdminPage } from "../AdminPage";
+import MyBookingSearch from "../MyBookingSearch";
 
 it("renders correctly", () => {
-  const {queryByTestId, queryByPlaceholderName} = render(<AdminPage/>)
+  const {queryByTestId} = render(<AdminPage/>)
 
   expect(queryByTestId("add-btn")).toBeTruthy;
 })
