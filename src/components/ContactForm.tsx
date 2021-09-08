fabca40b69d9211d00220c085654e1e545380675
emresorkun <emresorkun@gmail.com>
2021-09-08 10:07:12 +0200

    msgRecievedModal validation added and handle submit configured. Payload is still not working

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,15 +3,16 @@ import "./contactForm.css";
 import restaurantApi from "../api/restaurantApi";
 import nodemailer from 'nodemailer';
 import { MsgRecievedModal } from "../modals/MsgRecievedModal";
+import Utilities from "../Utilities";
 
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-      user: 'deblina4.se@gmail.com',
-      pass: 'frontend@2020',
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//       user: 'deblina4.se@gmail.com',
+//       pass: 'frontend@2020',
+//   },
+// });
 
 interface ContactFormProps {
   setIsSent?: any;
@@ -40,6 +41,11 @@ const ContactForm = (props: ContactFormProps) => {
   const [message, setMessage] = useState("");
   const [showMsgRecievedModal, setShowMsgRecievedModal] = useState(false);
 
+  const [errorName, setErrorName]= useState(false);
+  const [errorEmail, setErrorEmail]= useState(false);
+  const [errorMessage, setErrorMessage]= useState(false);
+
+
   function handleNameChange(event: any) {
     setName (event.target.value);
   }
@@ -52,13 +58,42 @@ const ContactForm = (props: ContactFormProps) => {
     setMessage (event.target.value);
   }
 
-
+  const validate= () : boolean=>{
+    let valid = true;
+    if(name === ""){
+        setErrorName(true);
+        valid = false;
+    }
+    else{
+        setErrorName(false);
+    }
+    if(!Utilities.validateEmail(email)){
+        setErrorEmail(true);
+        valid = false;
+    }
+    else{
+        setErrorEmail(false);
+    }
+    if(message === ""){
+      setErrorMessage(true);
+      valid = false;
+  }
+  else{
+      setErrorMessage(false);
+  }
+    return valid;
+}
   async function handleSubmit() {
+    if(!validate()){
+      return
+    }
     const payload = {
       name,
       email,
       message,
     };
+    
+    
 
     // function show() {
     //   //add here show msg recieved and set the boleeans
@@ -69,6 +104,8 @@ const ContactForm = (props: ContactFormProps) => {
     console.log(props.post)
 
     await props.post(payload);
+    //sends data?
+    setShowMsgRecievedModal(true)
     
 
   }
@@ -79,10 +116,12 @@ const ContactForm = (props: ContactFormProps) => {
     <div id="contact-container">
     <p>Please Contact Us Using the Form Below</p>
       <form >
+
+          {errorName ? <p style={{color : "orange", margin: 0}}>Please enter your name</p> : ''}
           <div><input type="text" value={name} placeholder="Name" required onChange={handleNameChange}/></div>
-        
+          {errorEmail ? <p style={{color : "orange", margin: 0}}>Please enter a valid email</p> : ''}
           <div><input type="email" value={email} placeholder="Email" required onChange={handleEmailChange}/></div>
-        
+          {errorMessage ? <p style={{color : "orange", margin: 0}}>Please enter a Message</p> : ''}
           <div><input type="text" value={message} placeholder="Message" required onChange={handleMessageChange}/></div>
  
           <button type="button" onClick={handleSubmit}>Send</button>
